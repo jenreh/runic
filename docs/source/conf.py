a6353df000ce41809e0bcd6eb24ebf1f1ba64c7f
Jens Rehpöhler <jens.rehpoehler@bertelsmann.de>
2026-06-07 23:58:52 +0200

    chore: release v0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@ on_rtd = os.environ.get("READTHEDOCS") == "True"
 project = "Runic"
 copyright = "2026, Jens Rehpöhler"
 author = "Jens Rehpöhler"
-release = "0.2.2"
-version = "0.2.2"
+release = "0.3.0"
+version = "0.3.0"
 
 # ReadTheDocs version handling
 if on_rtd:
